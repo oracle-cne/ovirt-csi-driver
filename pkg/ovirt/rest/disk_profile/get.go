232d718b2cf57f651bfd55c4b97e76884634c6ab
Paul Mackin <paul.mackin@oracle.com>
2025-07-22 15:29:41 -0400

    Add feature to select a storage domain given an OLVM disk profile name in the StorageClass (#25)
    Co-authored-by: Michael Gianatassio <31552226+mgianatagh@users.noreply.github.com>

---
+++ pkg/ovirt/rest/disk_profile/get.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2025, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package disk_profile
+
+import (
+	"fmt"
+	"github.com/ovirt/csi-driver/pkg/ovirt/ovclient"
+	"k8s.io/apimachinery/pkg/util/json"
+	log "k8s.io/klog"
+	"strings"
+)
+
+// GetDiskProfiles gets all disk profiles.
+func GetDiskProfiles(ovcli *ovclient.Client) (*DiskProfileList, error) {
+	const path = "/api/diskprofiles"
+
+	// call the server
+	body, err := ovcli.REST.Get(ovcli.AccessToken, path)
+	if err != nil {
+		err = fmt.Errorf("Error doing HTTP GET: %v", err)
+		return nil, err
+	}
+
+	diskProfileList := &DiskProfileList{}
+	err = json.Unmarshal(body, diskProfileList)
+	if err != nil {
+		err = fmt.Errorf("Error unmarshaling StorageDomains: %v", err)
+		log.Error(err)
+		return nil, err
+	}
+
+	return diskProfileList, nil
+}
+
+// GetDiskGetDiskProfilesByName gets a list disk profile by name
+func GetDiskProfilesByName(ovcli *ovclient.Client, profileName string) ([]*DiskProfile, error) {
+	matchList := []*DiskProfile{}
+
+	profileList, err := GetDiskProfiles(ovcli)
+	if err != nil {
+		return nil, err
+	}
+
+	for i, sd := range profileList.DiskProfiles {
+		if strings.EqualFold(sd.Name, profileName) {
+			matchList = append(matchList, &profileList.DiskProfiles[i])
+		}
+	}
+
+	return matchList, nil
+}
