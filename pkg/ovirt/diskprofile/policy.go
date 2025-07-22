b4e9177fb7c81893527015c123fee130532a3b23
Paul Mackin <paul.mackin@oracle.com>
2025-07-22 15:29:41 -0400

    Add feature to select a storage domain given an OLVM disk profile name in the StorageClass (#25)

---
+++ pkg/ovirt/diskprofile/policy.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2025, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package diskprofile
+
+import (
+	"fmt"
+	"github.com/ovirt/csi-driver/pkg/ovirt/rest/storagedomain"
+	"strconv"
+)
+
+const PolicyLeastUsed = "leastUsed"
+
+// Select the best domain using the policy
+// The current policy picks the storage domain that is least used (most space).
+func selectDomainUsingPolicy(domains []*storagedomain.StorageDomain, policy string) (*storagedomain.StorageDomain, error) {
+	switch policy {
+	case PolicyLeastUsed:
+		return selectLeastUsed(domains)
+	default:
+		return selectLeastUsed(domains)
+	}
+}
+
+func selectLeastUsed(domains []*storagedomain.StorageDomain) (*storagedomain.StorageDomain, error) {
+	var selected *storagedomain.StorageDomain
+	var maxsize int64
+	for i, d := range domains {
+		size, err := strconv.ParseInt(d.Available, 10, 64)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse disk size: %w", err)
+		}
+		if size > maxsize {
+			selected = domains[i]
+			maxsize = size
+		}
+	}
+	return selected, nil
+}
