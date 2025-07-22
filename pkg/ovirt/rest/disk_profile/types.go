dd819690e2bd00b04aa1261c6c9b47c5d66c20f7
Paul Mackin <paul.mackin@oracle.com>
2025-07-22 15:29:41 -0400

    Add feature to select a storage domain given an OLVM disk profile name in the StorageClass (#25)

---
+++ pkg/ovirt/rest/disk_profile/types.go
@@ -0,0 +1,30 @@
+// Copyright (c) 2025, Oracle and/or its affiliates.
+// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+package disk_profile
+
+const (
+	StatusOK = "ok"
+
+	FormatCow = "cow"
+
+	BackupNone = "none"
+)
+
+type DiskProfileList struct {
+	DiskProfiles []DiskProfile `json:"disk_profile"`
+}
+
+type DiskProfile struct {
+	StorageDomain struct {
+		Href string `json:"href"`
+		Id   string `json:"id"`
+	} `json:"storage_domain"`
+	Name string `json:"name"`
+	Link []struct {
+		Href string `json:"href"`
+		Rel  string `json:"rel"`
+	} `json:"link"`
+	Href string `json:"href"`
+	Id   string `json:"id"`
+}
